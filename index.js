596316730872b38936324d2f277407c0d08beece
jollen <jollen@moko365.com>
2016-02-26 20:21:47 +0800

    Fix legal name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  *
- *  .CITY Websocket Broker
- *  Copyright 2015 WoT.City Inc. All rights reserved.
+ *  Copyright 2015 WoT.City Open Source Project. All rights reserved.
+ *
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
